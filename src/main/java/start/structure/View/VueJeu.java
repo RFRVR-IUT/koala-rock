29b3534576b9ada3887f8eced755a567cfbfcea0
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-10 12:04:30 +0100

    Button Demarrage + Jeu

--- src/main/java/start/structure/View/VueJeu.java
+++ src/main/java/start/structure/View/VueJeu.java
@@ -6,7 +6,9 @@ import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -80,10 +82,10 @@ public class VueJeu {
         tonneau4.setLayoutY(-30);
         tonneau5.setLayoutY(-30);
 
-
         mario.setLayoutX(20 * 10);
         mario.setLayoutY(545);
 
+        //////////////// Label ///////////////////////
         Label score = new Label("Score : 0");
         score.getStyleClass().add("Score_Vie");
         score.setLayoutX(1080);
@@ -103,6 +105,57 @@ public class VueJeu {
         chrono.getStyleClass().add("Chrono");
         chrono.setLayoutX(20);
         chrono.setLayoutY(50);
+        //////////////// End Label ///////////////////////
+
+        //////////////// Button ///////////////////////
+
+        Button button_Bas = new Button();
+        ImageView image_Bas = new ImageView("file:src/main/resources/Button/Button_bas.png");
+        image_Bas.setFitHeight(50);
+        image_Bas.setFitWidth(50);
+        button_Bas.setGraphic(image_Bas);
+        button_Bas.getStyleClass().add("button_Action");
+        button_Bas.setLayoutX(225);
+        button_Bas.setLayoutY(480);
+
+        Button button_Haut = new Button();
+        ImageView image_Haut = new ImageView("file:src/main/resources/Button/Button_haut.png");
+        image_Haut.setFitHeight(50);
+        image_Haut.setFitWidth(50);
+        button_Haut.setGraphic(image_Haut);
+        button_Haut.getStyleClass().add("button_Action");
+        button_Haut.setLayoutX(225);
+        button_Haut.setLayoutY(415);
+
+        Button button_Gauche = new Button();
+        ImageView image_Gauche = new ImageView("file:src/main/resources/Button/Button_gauche.png");
+        image_Gauche.setFitHeight(50);
+        image_Gauche.setFitWidth(50);
+        button_Gauche.setGraphic(image_Gauche);
+        button_Gauche.getStyleClass().add("button_Action");
+        button_Gauche.setLayoutX(155);
+        button_Gauche.setLayoutY(480);
+
+        Button button_Droite = new Button();
+        ImageView image_Droite = new ImageView("file:src/main/resources/Button/Button_droite.png");
+        image_Droite.setFitHeight(50);
+        image_Droite.setFitWidth(50);
+        button_Droite.setGraphic(image_Droite);
+        button_Droite.getStyleClass().add("button_Action");
+        button_Droite.setLayoutX(296);
+        button_Droite.setLayoutY(480);
+
+        Button button_Espace = new Button();
+        ImageView image_Espace = new ImageView("file:src/main/resources/Button/Button_espace.png");
+        image_Espace.setFitHeight(50);
+        image_Espace.setFitWidth(200);
+        button_Espace.setGraphic(image_Espace);
+        button_Espace.getStyleClass().add("button_Action");
+        button_Espace.setLayoutX(150);
+        button_Espace.setLayoutY(550);
+
+        //////////////// End Button ///////////////////////
+
 
         AnimationTimer timer = new AnimationTimer() {
             private long timestamp;
@@ -171,10 +224,8 @@ public class VueJeu {
         // Couleur Interface
         interfaceJeu.setStyle("-fx-background-color: #000000;");
 
-        interfaceJeu.getChildren().add(score);
-        interfaceJeu.getChildren().add(vie);
-        interfaceJeu.getChildren().add(nomJeu);
-        interfaceJeu.getChildren().add(chrono);
+        interfaceJeu.getChildren().addAll(score, vie, nomJeu, chrono);
+        interfaceJeu.getChildren().addAll(button_Bas, button_Haut, button_Gauche, button_Droite, button_Espace);
 
 
         System.out.println(echelle1.getLayoutX());
