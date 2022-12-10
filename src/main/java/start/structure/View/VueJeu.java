fb3daeaddc31dcfcbc3c5478b7a5b5481f99fe75
Vanhove Valentin <valentin.vanhove@etu.umontpellier.fr>
2022-12-10 12:19:06 +0100

    button to label (Touche)

--- src/main/java/start/structure/View/VueJeu.java
+++ src/main/java/start/structure/View/VueJeu.java
@@ -109,7 +109,7 @@ public class VueJeu {
 
         //////////////// Button ///////////////////////
 
-        Button button_Bas = new Button();
+        Label button_Bas = new Label();
         ImageView image_Bas = new ImageView("file:src/main/resources/Button/Button_bas.png");
         image_Bas.setFitHeight(50);
         image_Bas.setFitWidth(50);
@@ -118,7 +118,7 @@ public class VueJeu {
         button_Bas.setLayoutX(150);
         button_Bas.setLayoutY(530);
 
-        Button button_Haut = new Button();
+        Label button_Haut = new Label();
         ImageView image_Haut = new ImageView("file:src/main/resources/Button/Button_haut.png");
         image_Haut.setFitHeight(50);
         image_Haut.setFitWidth(50);
@@ -127,7 +127,7 @@ public class VueJeu {
         button_Haut.setLayoutX(150);
         button_Haut.setLayoutY(465);
 
-        Button button_Gauche = new Button();
+        Label button_Gauche = new Label();
         ImageView image_Gauche = new ImageView("file:src/main/resources/Button/Button_gauche.png");
         image_Gauche.setFitHeight(50);
         image_Gauche.setFitWidth(50);
@@ -136,7 +136,7 @@ public class VueJeu {
         button_Gauche.setLayoutX(80);
         button_Gauche.setLayoutY(530);
 
-        Button button_Droite = new Button();
+        Label button_Droite = new Label();
         ImageView image_Droite = new ImageView("file:src/main/resources/Button/Button_droite.png");
         image_Droite.setFitHeight(50);
         image_Droite.setFitWidth(50);
@@ -145,7 +145,7 @@ public class VueJeu {
         button_Droite.setLayoutX(221);
         button_Droite.setLayoutY(530);
 
-        Button button_Espace = new Button();
+        Label button_Espace = new Label();
         ImageView image_Espace = new ImageView("file:src/main/resources/Button/Button_espace.png");
         image_Espace.setFitHeight(50);
         image_Espace.setFitWidth(200);
@@ -226,7 +226,6 @@ public class VueJeu {
 
         interfaceJeu.getChildren().addAll(score, vie, nomJeu, chrono);
         interfaceJeu.getChildren().addAll(button_Bas, button_Haut, button_Gauche, button_Droite, button_Espace);
-
 
         System.out.println(echelle1.getLayoutX());
 
